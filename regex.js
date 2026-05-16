498ee1828ec3a3dcce8c2e1ecae0e9b5bc71d1af
Nicolas Matheisen <nicolasmatheisen@icloud.com>
2026-05-16 22:01:20 +0200

    die deutschen begriffe in englische unbenannt

--- regex.js
+++ regex.js
@@ -7,20 +7,20 @@ function SPICENetlistValidator(UserInput) {
    const errors = [];
    //trim() entfernt Leerzeichen von beiden Seiten der Zeichenkette
    //split('\n') entfernt die Anweisung einer neuen Zeile (Enter oder new line)
-   const Zeile = UserInput.trim().split('\n');
+   const line = UserInput.trim().split('\n');
 
-   const Titel = Zeile[0].trim();
+   const title = line[0].trim();
    //testet ob der Titel mit einem Bauteil beginnt (/i ist egal ob die Buchstaben groß oder klein geschrrieben werden)
-   if(/^[RCL]/i.test(Titel)){
+   if(/^[RCL]/i.test(title)){
         errors.push("Die erste Zeile muss ein Titel sein, darf nicht mit einem Bauteil gebinnen");
    }
    return errors;
 }
 
-function ueberpruefen() {
+function check() {
     const UserInput = document.getElementById('UserInput').value;
     const errors = SPICENetlistValidator(UserInput);
-    const Verifizierungsergebnis = document.getElementById('Verifizierungsergebnis');
+    const Verifizierungsergebnis = document.getElementById('verifyingResult');
 
     if(errors.length === 0) {
         Verifizierungsergebnis.textContent ='Netlist ist gültig';
