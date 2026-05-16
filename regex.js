2861be3d27cfe2863986d11e89830055b21895f8
Nicolas Matheisen <nicolasmatheisen@icloud.com>
2026-05-16 03:30:47 +0200

    baue ein Validator für SPICE Netlist

---
+++ regex.js
@@ -0,0 +1,10 @@
+function SPICENetlistValidator(UserInput) {
+    /* 
+    Regel 1: die erste Zeile ist der Titel
+    Regel 2: Eingabe muss mit .end beendet werden
+    Regel 3: Syntaxanalyse der Bauteile
+    */
+   const errors = [];
+
+   return errors;
+}
